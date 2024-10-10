5a2b2e5ed4841759988d17233ef14e6512ab05aa
adevancomp <adevancomp@gmail.com>
2024-10-10 16:49:14 -0400

    implementar navegação de tela de seleção de função para login com ícone customizado

--- ecopoints/app/src/main/java/br/edu/uea/ecopoints/screen/LoginActivity.kt
+++ ecopoints/app/src/main/java/br/edu/uea/ecopoints/screen/LoginActivity.kt
@@ -1,21 +1,38 @@
 package br.edu.uea.ecopoints.screen
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import br.edu.uea.ecopoints.R
 import br.edu.uea.ecopoints.databinding.ActivityLoginBinding
-import br.edu.uea.ecopoints.databinding.ActivityMainBinding
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var imageViewRoleIcon: ImageView
+    private lateinit var edtEmail: TextInputEditText
+    private lateinit var edtPassword: TextInputEditText
+    private lateinit var clCreateNewAccount: ConstraintLayout
+    private lateinit var btLogin: MaterialButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupView()
+        val role: String = intent.getStringExtra("roleSelected") ?: ""
+        if(role.isNotBlank()){
+            when(role){
+                "employee" -> imageViewRoleIcon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.worker_icon))
+                "admin" -> imageViewRoleIcon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.admin_icon))
+                "driver" -> imageViewRoleIcon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.driver_icon))
+                else -> imageViewRoleIcon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.worker_icon))
+            }
+        }
         setupListeners()
     }
 
@@ -24,6 +41,9 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun setupView() {
-
+        imageViewRoleIcon = binding.ivRole
+        edtEmail = binding.edtEmail
+        edtPassword = binding.edtPassword
+        clCreateNewAccount = binding.clCreateAccountText
     }
 }
--- ecopoints/app/src/main/java/br/edu/uea/ecopoints/screen/MainActivity.kt
+++ ecopoints/app/src/main/java/br/edu/uea/ecopoints/screen/MainActivity.kt
@@ -1,40 +1,46 @@
 package br.edu.uea.ecopoints.screen
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import br.edu.uea.ecopoints.databinding.ActivityLoginBinding
 import br.edu.uea.ecopoints.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var imageEmployee: ImageView
     private lateinit var imageAdmin: ImageView
     private lateinit var imageDriver: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //setupView()
-        //setupListeners()
+        setupView()
+        setupListeners()
     }
 
     private fun setupListeners() {
         imageEmployee.setOnClickListener {
-
+            val intent: Intent = Intent(this, LoginActivity::class.java)
+            intent.putExtra("roleSelected","employee")
+            startActivity(intent)
         }
         imageAdmin.setOnClickListener {
-
+            val intent: Intent = Intent(this, LoginActivity::class.java)
+            intent.putExtra("roleSelected","admin")
+            startActivity(intent)
         }
         imageDriver.setOnClickListener {
-
+            val intent: Intent = Intent(this, LoginActivity::class.java)
+            intent.putExtra("roleSelected","driver")
+            startActivity(intent)
         }
     }
 
     private fun setupView() {
-        /*imageEmployee = binding.ivEmployee
+        imageEmployee = binding.ivEmployee
         imageAdmin = binding.ivAdmin
-        imageDriver = binding.ivDriver*/
+        imageDriver = binding.ivDriver
     }
 }
