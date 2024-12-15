49a435ed78b5d3c3f1b1c13695209687d2b5b0f4
adevancomp <adevancomp@gmail.com>
2024-12-15 15:28:55 -0400

    implementa paging source de materials reciclados

---
+++ app/app/src/main/java/br/edu/uea/ecopoints/screen/home/employee/fragment/recyclerview/SeparatedMaterialPagingSource.kt
@@ -0,0 +1,46 @@
+package br.edu.uea.ecopoints.screen.home.employee.fragment.recyclerview
+
+import android.util.Log
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import br.edu.uea.ecopoints.data.api.EcoApi
+import br.edu.uea.ecopoints.domain.entity.SeparatedMaterial
+import java.io.IOException
+
+class SeparatedMaterialPagingSource (
+    private val ecoApi: EcoApi,
+    private val employeeId: Long
+): PagingSource<Int, SeparatedMaterial>(){
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SeparatedMaterial> {
+        val page = params.key ?: 0
+        return try {
+            val response = ecoApi.getAllSpMaterialByEmployeeId(
+                employeeId,
+                page = page,
+                size = params.loadSize
+            )
+            if (response.isSuccessful && response.body() != null) {
+                Log.i("ECO","Recuperou \n ${response.body()}")
+                val responseBody = response.body()!!
+                LoadResult.Page(
+                    data = responseBody.content,
+                    prevKey = if (page == 0) null else page - 1,
+                    nextKey = if (responseBody.last) null else page + 1
+                )
+            } else {
+                Log.i("ECO","Erro ao recupear lista \n ${response.code()}")
+                LoadResult.Error(Throwable("Erro na resposta da API"))
+            }
+        } catch (e: IOException) {
+            Log.i("ECO","ERRO de IO ${e.message}")
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, SeparatedMaterial>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+}
