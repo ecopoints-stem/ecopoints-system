679f30495ea404ef019707612db36f2cf46416b5
adevancomp <adevancomp@gmail.com>
2024-12-16 16:13:43 -0400

    adiciona protótipo de função update employee

--- app/app/src/main/java/br/edu/uea/ecopoints/screen/home/employee/viewmodel/EditProfileEmployeeViewModel.kt
+++ app/app/src/main/java/br/edu/uea/ecopoints/screen/home/employee/viewmodel/EditProfileEmployeeViewModel.kt
@@ -1,8 +1,12 @@
 package br.edu.uea.ecopoints.screen.home.employee.viewmodel
 
 import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.edu.uea.ecopoints.data.api.EcoApi
+import br.edu.uea.ecopoints.domain.network.request.EmployeeUpdate
+import br.edu.uea.ecopoints.screen.home.employee.state.EditProfileEmployeeState
 import com.fasterxml.jackson.databind.ObjectMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -13,5 +17,14 @@ class EditProfileEmployeeViewModel @Inject constructor(
     private val shared: SharedPreferences,
     private val mapper: ObjectMapper
 ) : ViewModel() {
+    private val _state = MutableLiveData<EditProfileEmployeeState>()
+    val state : LiveData<EditProfileEmployeeState> = _state
 
+    fun updateEmployee(
+        name: String, email: String,
+        phone: String?, cnpjCooperative: String?,
+        password: String
+    ) {
+
+    }
 }
