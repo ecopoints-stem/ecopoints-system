0c116c9f08af4b49bda508029333b0bd52a61d19
adevancomp <adevancomp@gmail.com>
2024-12-21 09:52:20 -0400

    muda tipo de dado de totalsEmployees

--- app/app/src/main/java/br/edu/uea/ecopoints/domain/network/response/BarData.kt
+++ app/app/src/main/java/br/edu/uea/ecopoints/domain/network/response/BarData.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDate
 
 data class BarData(
-    @JsonProperty("totalsEmployees") val totalsEmployees: Int,
+    @JsonProperty("totalsEmployees") val totalsEmployees: Long,
     @JsonProperty("cooperativeName") val cooperativeName: String,
     @JsonProperty("startDate") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") val startDate: LocalDate,
     @JsonProperty("endDate") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") val endDate: LocalDate,
