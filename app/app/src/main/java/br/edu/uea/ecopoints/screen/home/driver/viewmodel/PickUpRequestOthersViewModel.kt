d8556253bea1e68741d32f261a3601401c43c923
adevancomp <adevancomp@gmail.com>
2024-12-25 11:30:22 -0400

    implementa view model

--- app/app/src/main/java/br/edu/uea/ecopoints/screen/home/driver/viewmodel/PickUpRequestOthersViewModel.kt
+++ app/app/src/main/java/br/edu/uea/ecopoints/screen/home/driver/viewmodel/PickUpRequestOthersViewModel.kt
@@ -1,4 +1,26 @@
 package br.edu.uea.ecopoints.screen.home.driver.viewmodel
 
-class PickUpRequestOthersViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import br.edu.uea.ecopoints.data.api.EcoApi
+import br.edu.uea.ecopoints.domain.entity.PickUpRequest
+import br.edu.uea.ecopoints.domain.entity.enums.PickupRequestStatus
+import br.edu.uea.ecopoints.screen.home.driver.fragment.recyclerview.request.PickUpRequestOthersPagingSource
+
+class PickUpRequestOthersViewModel (
+    private val ecoApi: EcoApi,
+    private val driverId: Long,
+    private val statuses: List<String>
+) : ViewModel() {
+    val pickups = Pager(PagingConfig(pageSize = 3)){
+        PickUpRequestOthersPagingSource(ecoApi,driverId, statuses)
+    }.flow.cachedIn(viewModelScope)
+
+    suspend fun updatePickUpStatus(pickUpId: Long, newStatus: PickupRequestStatus): PickUpRequest? {
+        val response = ecoApi.updatePickUpStatus(pickUpId, newStatus)
+        return if(response.isSuccessful) response.body() else null
+    }
 }
--- app/app/src/main/java/br/edu/uea/ecopoints/screen/home/driver/viewmodel/factory/PickUpRequestOthersViewModelFactory.kt
+++ app/app/src/main/java/br/edu/uea/ecopoints/screen/home/driver/viewmodel/factory/PickUpRequestOthersViewModelFactory.kt
@@ -1,4 +1,21 @@
 package br.edu.uea.ecopoints.screen.home.driver.viewmodel.factory
 
-class PickUpRequestOthersViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import br.edu.uea.ecopoints.data.api.EcoApi
+import br.edu.uea.ecopoints.screen.home.driver.viewmodel.PickUpRequestOthersViewModel
+
+class PickUpRequestOthersViewModelFactory(
+    private val ecoApi: EcoApi,
+    private val driverId: Long,
+    private val statuses: List<String>
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        if(modelClass == PickUpRequestOthersViewModel::class.java){
+            return PickUpRequestOthersViewModel(ecoApi, driverId, statuses) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel instance for $modelClass")
+    }
 }
